9439a45bc39fc8445540b80cb8775f61b556fb5a
Sai Dushyant <126763864+SaiDushyant@users.noreply.github.com>
2024-12-13 19:06:22 +0530

    Updated index.js for routing in github

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom"; // Import HashRouter
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      {" "}
+      {/* Wrap your App with HashRouter */}
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
