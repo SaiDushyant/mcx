8bd1a03fffb00c901626194a0831d04e74f8404b
Sai Dushyant <126763864+SaiDushyant@users.noreply.github.com>
2024-12-13 19:27:02 +0530

    Did the basic structure of the site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { About, Gallery, Home, Services } from "./pages"; // Importing from pages/index.js
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {" "}
+      {/* Wrap the app with BrowserRouter */}
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link> {/* Link to the home page */}
+              </li>
+              <li>
+                <Link to="/about">About</Link> {/* Link to the about page */}
+              </li>
+              <li>
+                <Link to="/gallery">Gallery</Link>{" "}
+                {/* Link to the gallery page */}
+              </li>
+              <li>
+                <Link to="/services">Services</Link>{" "}
+                {/* Link to the services page */}
+              </li>
+            </ul>
+          </nav>
+
+          {/* Define the routes here */}
+          <Routes>
+            <Route path="/" element={<Home />} /> {/* Home page route */}
+            <Route path="/about" element={<About />} /> {/* About page route */}
+            <Route path="/gallery" element={<Gallery />} />{" "}
+            {/* Gallery page route */}
+            <Route path="/services" element={<Services />} />{" "}
+            {/* Services page route */}
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Gallery.js
@@ -0,0 +1,12 @@
+// src/Gallery.js
+import React from "react";
+
+const Gallery = () => {
+  return (
+    <div>
+      <h1>Gallery Page</h1>
+    </div>
+  );
+};
+
+export default Gallery;
