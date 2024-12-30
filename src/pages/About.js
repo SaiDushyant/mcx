23e94a88cb7580801039f3199c9373c6f6e4e1d7
Sai Dushyant <126763864+SaiDushyant@users.noreply.github.com>
2024-12-30 14:19:56 +0530

    Added Founders section in about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,7 @@
 // src/About.js
 import React from "react";
 import { AboutBackgroundImage } from "../assets/images/images";
+import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 
 const About = () => {
@@ -46,12 +47,12 @@ const About = () => {
       {/* Come join us */}
 
       <section className="w-full h-[80vh] flex justify-center items-center">
-        <div className="w-full h-[60vh] bg-green-200 text-center">
-          <div className="md:mx-96 xl:mx-[25rem] mt-28">
-            <h1 className="text-[2rem] md:text-7xl font-extrabold md:font-bold">
+        <div className="w-full h-[60vh] bg-green-200 text-center flex justify-center items-center">
+          <div className="md:mx-96 xl:mx-[24rem]">
+            <h1 className="text-5xl md:text-6xl xl:text-7xl font-extrabold md:font-bold">
               COME JOIN WITH <span style={{ color: "#FFA500" }}>US!</span>
             </h1>
-            <p className="text-base md:text-xl mt-10 font-medium">
+            <p className="text-base md:text-2xl mt-5 md:mt-10 font-medium">
               Be a part of a transformative journey that empowers farmers and
               connects investors to meaningful opportunities. At MCX Foundation,
               we create sustainable solutions that drive growth, foster
@@ -61,6 +62,47 @@ const About = () => {
           </div>
         </div>
       </section>
+
+      {/* Meet our Founders */}
+
+      <section className="py-10">
+        <div className="text-center mb-10">
+          <h2 className="text-3xl font-bold md:text-6xl text-gray-800">
+            Meet <span style={{ color: "#FFA500" }}>Our</span> Founders
+          </h2>
+        </div>
+
+        <div className="flex flex-col md:flex-row justify-center items-center space-y-8 md:space-y-0 md:space-x-24">
+          {/* Founder 1 */}
+          <div className="w-72 md:w-80 bg-white shadow-xl rounded-3xl p-6 flex flex-col items-center text-center hover:scale-105 duration-300">
+            <div className="w-60 md:w-72 h-60 md:h-72 rounded-full bg-gray-300 mb-4"></div>
+            <h3 className="text-xl font-semibold text-gray-800">
+              NETHAJI SUBASH CHANDRA BOSE S
+            </h3>
+            <p className="text-gray-600">MEMBER TRUSTEE</p>
+          </div>
+
+          {/* Founder 2 */}
+          <div className="w-72 md:w-80 bg-white shadow-xl rounded-3xl p-6 flex flex-col items-center text-center md:scale-105 hover:scale-105 md:hover:scale-110 duration-300">
+            <div className="w-60 md:w-72 h-60 md:h-72 rounded-full bg-gray-300 mb-4"></div>
+            <h3 className="text-xl font-semibold text-gray-800">SIVA C</h3>
+            <p className="text-gray-600">FOUNDER CUM MEMBER TRUSTEE</p>
+          </div>
+
+          {/* Founder 3 */}
+          <div className="w-72 md:w-80 bg-white shadow-xl rounded-3xl p-6 flex flex-col items-center text-center md:scale-105 hover:scale-105 md:hover:scale-110 duration-300">
+            <div className="w-60 md:w-72 h-60 md:h-72 rounded-full bg-gray-300 mb-4"></div>
+            <h3 className="text-xl font-semibold text-gray-800">
+              VIJAYAPRAKASH M
+            </h3>
+            <p className="text-gray-600">MEMBER TRUSTEE</p>
+          </div>
+        </div>
+      </section>
+
+      {/* Footer */}
+
+      <Footer />
     </>
   );
 };
