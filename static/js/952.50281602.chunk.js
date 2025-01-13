da03017ec9d8c3fd606b5bd498de4896fbe88968
SaiDushyant <SaiDushyant@users.noreply.github.com>
2025-01-13 18:55:07 +0000

    deploy: 3eaa7905460565c3e95a544d3bc181920e4cc73a

---
+++ static/js/952.50281602.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmcx=self.webpackChunkmcx||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.50281602.chunk.js.map
