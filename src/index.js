49f60b046c00badc1d40f56c6006db9c4e871f56
Sai Dushyant <126763864+SaiDushyant@users.noreply.github.com>
2025-01-13 16:04:36 +0530

    Minor changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Router } from "react-router-dom"; // Import HashRouter
+import { BrowserRouter } from "react-router-dom"; // Import HashRouter
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
@@ -8,9 +8,9 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter>
       <App /> {/* Wrap your App with HashRouter */}
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
