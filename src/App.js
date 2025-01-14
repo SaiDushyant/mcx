eab1b69c5d2034c89fbb3bb64d9b9c98604e0421
Sai Dushyant <126763864+SaiDushyant@users.noreply.github.com>
2025-01-14 22:25:04 +0530

    Updated the defaut behavior of react in redirecting and scrolling to the top of the page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import { About, Admin, Gallery, Home, Login, Services } from "./pages"; // Importing from pages/index.js
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
